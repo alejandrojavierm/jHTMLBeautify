a2ead303aaad2e5e9504077937211f8e20c0dd45
alejandrojm <alejandro.javier.m@gmail.com>
2019-06-08 22:27:20 -0300

    Documentation and new functions based on JSoup
    Fix CleanXML2 format, adding code examples and a new jSoup FormatXML , FormatHTML and FormatHTMLBody functions.

--- src/jtidy/b4j/htmlbeautify/HTMLBeautify.java
+++ src/jtidy/b4j/htmlbeautify/HTMLBeautify.java
@@ -4,16 +4,28 @@ package jtidy.b4j.htmlbeautify;
 import anywheresoftware.b4a.BA.*;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.StringReader;
+import java.io.StringWriter;
+
 import org.w3c.tidy.Tidy;
 
-@Version(0.1f)
+import org.jsoup.Jsoup;
+import org.jsoup.parser.Parser;
+import org.jsoup.nodes.Document;
+
+@Version(0.2f)
 @ShortName("HTMLBeautify")
-@DependsOn(values={"jHTMLBeautify_Jtidy"})
+@DependsOn(values={"jHTMLBeautify_Jtidy","jHTMLBeautify_Jsoup"})
 public class HTMLBeautify {
 	
 	/**
 	 * This function clean an XML data using UTF-8 encoding
 	 * if an exception is launched the result will be "Error to parse the XML"
+	 * Example:<code>
+	 * Dim objHTMLBeautify as HTMLBeautify
+	 * Dim xml as String = File.ReadString(File.DirApp,"demo.html")
+	 * xml = objHTMLBeautify.CleanXML(xml)
+	 * Log(xml)</code>
 	 */
 	public String CleanXML(String data){
 		try {
@@ -36,6 +48,11 @@ public class HTMLBeautify {
 	/**
 	 * This function clean an HTML data using UTF-8 encoding,
 	 * if an exception is launched the result will be "Error to parse the HTML"
+	 * Example:<code>
+	 * Dim objHTMLBeautify as HTMLBeautify
+	 * Dim html as String = File.ReadString(File.DirApp,"demo.html")
+	 * html = objHTMLBeautify.CleanHTML(html)
+	 * Log(html)</code>
 	 */
 	public String CleanHTML(String data)  {
 		try {
@@ -55,6 +72,11 @@ public class HTMLBeautify {
 	/**
 	 * This function clean an HTML data using an encoding passed by value,
 	 * if an exception is launched the result will be "Error to parse the HTML"
+	 * Example:<code>
+	 * Dim objHTMLBeautify as HTMLBeautify
+	 * Dim html as String = File.ReadString(File.DirApp,"demo.html")
+	 * html = objHTMLBeautify.CleanHTML2(html,"UTF-8")
+	 * Log(html)</code>
 	 */
 	public String CleanHTML2(String data, String encoding)  {
 		try {
@@ -74,23 +96,79 @@ public class HTMLBeautify {
 	/**
 	 * This function clean an XML data using an encoding passed by value
 	 * if an exception is launched the result will be "Error to parse the XML"
+	 * Example:<code>
+	 * Dim objHTMLBeautify as HTMLBeautify
+	 * Dim xml as String = File.ReadString(File.DirApp,"demo.html")
+	 * xml = objHTMLBeautify.CleanXML2(xml,"UTF-8")
+	 * Log(xml)</code>
 	 */
 	public String CleanXML2(String data, String encoding)  {
 		try {
 	    Tidy tidy = new Tidy();
-	    tidy.setInputEncoding("UTF-8");
-	    tidy.setOutputEncoding("UTF-8");
+	    tidy.setInputEncoding(encoding);
+	    tidy.setOutputEncoding(encoding);
 	    tidy.setWraplen(Integer.MAX_VALUE);
 	    tidy.setPrintBodyOnly(true);
 	    tidy.setXmlOut(true);
 	    tidy.setSmartIndent(true);
-	    ByteArrayInputStream inputStream = new ByteArrayInputStream(data.getBytes("UTF-8"));
+	    ByteArrayInputStream inputStream = new ByteArrayInputStream(data.getBytes(encoding));
 	    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 	    tidy.parseDOM(inputStream, outputStream);
-	    return outputStream.toString("UTF-8");
+	    return outputStream.toString(encoding);
 		} catch (Exception e) {
 			return "Error to parse the XML";
 		}
+	}
+	
+	/**
+	 * This function parse XML using the Jsoup library
+	 * Example:<code>
+	 * Dim objHTMLBeautify as HTMLBeautify
+	 * Dim xml as String = File.ReadString(File.DirApp,"demo.html")
+	 * xml = objHTMLBeautify.FormatXML(xml)
+	 * Log(xml)</code>
+	 */
+	public static String FormatXML(String html){
+		try {
+	        Document doc = Jsoup.parse(html, "", Parser.xmlParser());
+	        return doc.toString();
+		} catch (Exception e) {
+        	return "Error to parse the XML";
+        }	
+	}
+	
+	/**
+	 * This function parse HTML body segment using the Jsoup library
+	 * Example:<code>
+	 * Dim objHTMLBeautify as HTMLBeautify
+	 * Dim xml as String = File.ReadString(File.DirApp,"demo.html")
+	 * xml = objHTMLBeautify.FormatXML(xml)
+	 * Log(xml)</code>
+	 */
+	public static String FormatHTMLBody(String html){
+		try {
+	        Document doc = Jsoup.parseBodyFragment(html);
+	        return doc.toString();
+		} catch (Exception e) {
+        	return "Error to parse the HTML Body Fragment";
+        }	
+	}
+	
+	/**
+	 * This function parse HTML using the Jsoup library
+	 * Example:<code>
+	 * Dim objHTMLBeautify as HTMLBeautify
+	 * Dim xml as String = File.ReadString(File.DirApp,"demo.html")
+	 * xml = objHTMLBeautify.FormatXML(xml)
+	 * Log(xml)</code>
+	 */
+	public static String FormatHTML(String html){
+		try {
+			Document doc = Jsoup.parse(html, "", Parser.htmlParser());
+	        return doc.toString();
+		} catch (Exception e) {
+        	return "Error to parse the HTML";
+        }	
 	}
 	
 }
